19f8bb96fa2b45a58b298f4e87019dee20d9ddca
robertolatini31 <robertolatini31@gmail.com>
2022-03-31 14:14:21 +0200

    added snack-3

---
+++ snack-3/main.js
@@ -0,0 +1,21 @@
+// Snack3
+// Generatore di “nomi cognomi” casuali: 
+// il Grande Gatsby ha una lista di nomi e una lista di cognomi, 
+// e da queste vuole generare una falsa lista di invitati 
+// con nome e cognome.
+
+// lista nomi
+let nameList = ['Roberto', 'Sandro', 'Marisa', 'Valeria'];
+console.log(`lista nomi: ${nameList}`);
+// lista cognomi
+let surnameList = ['Rossi', 'Latini', 'Gugliotti', 'Sozzi'];
+console.log(`lista cognomi: ${surnameList}`);
+// inizializzo la nuova lista
+let newList = [];
+
+for (let i = 0; (i < nameList.length && i < surnameList.length); i++) {
+    let element = nameList[i] + ' ' + surnameList[i];
+    newList[i] = element;
+}
+
+console.log(`La falsa lista invitati è: ${newList}`);
