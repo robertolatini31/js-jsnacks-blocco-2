be5b95e6d6416d58981d49d4b9e22c5391cb5715
robertolatini31 <robertolatini31@gmail.com>
2022-03-31 12:48:53 +0200

    added snack-1-for-while

---
+++ snack-1/snack-1-for/main.js
@@ -0,0 +1,20 @@
+// SNACK 1 
+// Il software deve chiedere per 5 volte all’utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti.
+
+// inizializzo array numeri utente
+let numbersUserArray =[];
+
+// inizializzo la somma
+let somma = 0;
+
+// chiedo 5 numeri all'utente
+for (let index = 0; index < 5; index++) {
+     numbersUserArray[index] = parseInt(prompt('inserire un numero'));
+     console.log(`il num inserito è: ${numbersUserArray[index]}`);
+    somma += numbersUserArray[index];
+}
+
+// stampo la somma
+
+console.log(`la somma è ${somma}`);
