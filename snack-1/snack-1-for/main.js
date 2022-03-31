51ba801ffd22617c081dd92601ffb7d8f29ab99d
robertolatini31 <robertolatini31@gmail.com>
2022-03-31 13:40:14 +0200

    adjusted structure

--- snack-1/snack-1-for/main.js
+++ snack-1/snack-1-for/main.js
@@ -8,12 +8,24 @@ let numbersUserArray =[];
 // inizializzo la somma
 let somma = 0;
 
+//for
 // chiedo 5 numeri all'utente
 for (let index = 0; index < 5; index++) {
      numbersUserArray[index] = parseInt(prompt('inserire un numero'));
      console.log(`il num inserito è: ${numbersUserArray[index]}`);
     somma += numbersUserArray[index];
 }
+
+
+
+// while
+// let index = 0
+// while (index < 5) {
+//      numbersUserArray[index] = parseInt(prompt('inserire un numero'));
+//      console.log(`il num inserito è: ${numbersUserArray[index]}`);
+//     somma += numbersUserArray[index];
+//     index++
+// }
 
 // stampo la somma
 
